a871f6b99e405da34009556dedf22069b30a0d93
palpitate <xus1758011080@outlook.com>
2022-09-06 13:03:52 +0800

    initial program

---
+++ OFS_backend/src/main/java/com/example/ofs_backend/OfsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ofs_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfsBackendApplication.class, args);
+    }
+
+}
---
+++ OFS_backend/src/test/java/com/example/ofs_backend/OfsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ofs_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
