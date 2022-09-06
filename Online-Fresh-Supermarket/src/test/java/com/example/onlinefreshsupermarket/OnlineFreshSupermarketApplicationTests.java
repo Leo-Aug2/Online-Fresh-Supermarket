a30724022de8d1f9ba6069f258196edd30aaac92
palpitate <xus1758011080@outlook.com>
2022-09-06 12:47:26 +0800

    initial program

---
+++ Online-Fresh-Supermarket/src/test/java/com/example/onlinefreshsupermarket/OnlineFreshSupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinefreshsupermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFreshSupermarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
