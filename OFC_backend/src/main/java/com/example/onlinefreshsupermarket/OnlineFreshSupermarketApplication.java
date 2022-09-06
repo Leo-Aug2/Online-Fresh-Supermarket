c95daef515562a4fce7be70c3a58320354fa2ce3
palpitate <xus1758011080@outlook.com>
2022-09-06 12:57:31 +0800

    initial program

---
+++ OFC_backend/src/main/java/com/example/onlinefreshsupermarket/OnlineFreshSupermarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinefreshsupermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFreshSupermarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineFreshSupermarketApplication.class, args);
+    }
+
+}
